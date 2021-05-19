4995a341d35a040cf6741d62b8436ede2496e6c6
Gabriel Carneiro <gabrielarruda08@gmail.com>
2021-05-19 20:35:16 -0300

    remove delay add getBRKM

--- src/models/Accidents.js
+++ src/models/Accidents.js
@@ -1,31 +1,21 @@
-//const ObjectId = require('mongodb').ObjectId;
-//const mongoose = require('mongoose');
 const ObjectId = require('mongodb').ObjectID;
 const mongo = require('../../bd');
-const VgeoAPI = require ('./VgeoAPI');
-const fs = require('fs');
+const Vgeo = require('./Vgeo');
 
 
-function delay(t, val) {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            resolve(val);
-        }, t);
-    });
- }
 
 class Accidents {
 
     fitParams(params) {
-        if(params.data){
+        if (params.data) {
             params.data = new Date(params.data);
         }
         return params;
     }
 
-    //function call connet to Mongo
+    //connect to Mongo
     async connect(){
-        try{
+        try {
             await mongo.connect();
             return mongo.db('accidents_map').collection('accidents');
         } catch (err) {
@@ -33,14 +23,17 @@ class Accidents {
         }
     }
 
-
-    async getCoordinates({ br , km }) {
+    // busca coordenadas de acordo com br e km
+    async getCoordinates({ br, km }) {
         try {
-            const vgeoapi = new VgeoAPI();//{ br , uf, data, km }
-            const data = "2021-2-28";
-            const uf = "PR";
-            const object = { br , uf, data, km };
-            const { geometry: { coordinates } } = await vgeoapi.coordenadas( object );
+            const vgeo = new Vgeo(); //{ br , uf, data, km }
+
+            const object = { br, uf, data, km };
+            const {
+                geometry: {
+                    coordinates
+                }
+            } = await vgeo.coordenadas(object);
 
             return coordinates
 
@@ -48,53 +41,56 @@ class Accidents {
             return `erro getCoordinates ${err}`
         }
     }
-
+    // insere no banco coordenadas de lista de acidentes passados por parametro
     async getAccidents(params) {
         try {
-            const vgeoapi = new VgeoAPI();
+            const vgeo = new Vgeo();
             const connection = await this.connect();
 
             const response = connection
                 .find(this.fitParams(params))
                 .forEach(async (dados) => {
-                        const { data } = dados;
-                        dados.data = '2021-2-21';
-                        const result = await vgeoapi.coordenadas(dados);
-                        if(result){
+                    const {
+                        data
+                    } = dados;
+                    dados.data = new Date().toISOString().slice(0, 10);
+                    const result = await vgeo.coordenadas(dados);
+                    if (result) {
 
-                            // fs.writeFileSync("../coordinates.json", JSON.stringify(result)), (err) => {
-                            //     if (err) {
-                            //         console.error(err);
-                            //         return;
-                            // }; console.log("File has been created");
+                        connection.updateOne({
+                            _id: ObjectId(dados._id)
+                        }, {
+                            $set: {
+                                latitude: result.geometry.coordinates[0][1],
+                                longitude: result.geometry.coordinates[0][0],
+                            }
+                        });
 
-                            connection.updateOne({
-                                _id: ObjectId(dados._id)
-                            }, {
-                                $set: {
-                                    latitude: result.geometry.coordinates[0][1],
-                                    longitude: result.geometry.coordinates[0][0],
-                                }
-                            });
+                    } else {
+                        return ("Erro ao conectar API");
+                    }
 
-                        }else{
-                            return ("Erro ao conectar API");
-                        }
-
-                        return response
-            });
+                    return response
+                });
         } catch (err) {
             return `erro getAccidents ${err}`
         }
     }
-
+// retorna lista de acidentes de acordo com os parametros informados
     async getListAccidents(params) {
         try {
             const connection = await this.connect();
             const response = connection
-                .aggregate([
-                    { $match: this.fitParams(params) },
-                    { $project: { _id: 0, latitude: 1, longitude: 1 } }
+                .aggregate([{
+                        $match: this.fitParams(params)
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            latitude: 1,
+                            longitude: 1
+                        }
+                    }
                 ])
                 .toArray()
             return response
@@ -103,6 +99,25 @@ class Accidents {
             return `erro getListAccidentes ${err}`
         }
     }
+    //retorna BR + km das coordenadas informada
+    /*[{"id":"1","br":"376","sg_tp_trecho":"B","uf":"PR","versao":"202101A","id_trecho":"","km":"577.85770650318477","lat":"-25.443142372410431","lng":"-49.453852674432397"},
+        {"id":"2","br":"277","sg_tp_trecho":"B","uf":"PR","versao":"202101A","id_trecho":"","km":"117.2422934968214","lat":"-25.443142372410431","lng":"-49.453852674432397"}]*/
+    async getBRKM({ lat, lng }){
+        try{
+            const vgeo = new Vgeo();
+            const obj = { lat, lng };
+            const response =  await vgeo.endereco(obj);
+            const endereco = response.map(({ br, km }) => ({ br, km }));
+        
+            return endereco
+
+        }catch(err){
+            return `erro getEnderecoBRKM ${err}`
+        }
+    }
+
+
+    
 
 }
 
